07c6b6c680099461f15d4ae5614b190c6c37bb99
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-04-06 21:59:53 +0900

    MENU、名刺画面の実装

--- app/src/main/java/com/example/hondanaoyuki/outofbusinesscards/MainActivity.kt
+++ app/src/main/java/com/example/hondanaoyuki/outofbusinesscards/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.hondanaoyuki.outofbusinesscards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_edit.*
+import android.content.Intent
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).apply {
+            val company = getString("company","")
+            val postal = getString("postal","")
+            val address = getString("address","")
+            val tel = getString("tel","")
+            val fax = getString("fax","")
+            val email = getString("email","")
+            val url = getString("url","")
+            val position = getString("position","")
+            val name = getString("name","")
+
+            companyText.setText(company)
+            postalText.setText(postal)
+            addressText.setText(address)
+            telText.setText(tel)
+            faxText.setText(fax)
+            emailText.setText(email)
+            urlText.setText(url)
+            positionText.setText(position)
+            nameText.setText(name)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.edit -> {
+                //
+                val intent = Intent(this,EditActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
